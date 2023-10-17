001b7bddac46e41af1c0993a1670e6758fe62561
jordan-manus <jlmanus17@gmail.com>
2023-10-17 15:55:40 -0400

    fixed call to get class name for container

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-let customerContainer = document.querySelector('.customerContainer');
+let customerContainer = document.getElementsByClassName('.customerContainer');
+if(customerContainer){
+  console.log('success');
+} else {
+  console.log('fail')
+}
 
-function buildCustomers(customers) {
+function quickList(customerArray){
+  for (let customer of customerArray) {
+    console.log(`this is: ${customer.name.first} ${customer.name.last}`);
+    console.log(customer.name.first);
+    }
+    // console.log(`this is for ${customer['name']}`);
+    // console.log(`this is their email: ${customer['email']}`);
+  }
+
+
+function buildCustomers(customerArray) {
   for (let customer in customers) {
     let box = document.createElement('div');
     box.classList.add('customer');
     let customerName = document.createElement('h2');
-    customerName.innerText = customer.name;
+    customerName.innerText =   `Name: ${customer.name.title}  ${customer.name.first}  ${customer.name.last}`;
+    // customerName.innerText = 'hello';
     box.appendChild(customerName);
     customerContainer.appendChild(box);
+    // customerContainer.appendChild(customerName)
   }
 }
 
+quickList(customers);
 buildCustomers(customers);
